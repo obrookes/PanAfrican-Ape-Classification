84dd08904a9614cc662da40b4939bce338b64859
obrookes <dl18206@bristol.ac.uk>
2021-11-29 15:58:05 +0000

    several changes including .detach() warning fix

--- lightning_train.py
+++ lightning_train.py
@@ -33,106 +33,22 @@ class EnsembleModel(pl.LightningModule):
 
         self.input_sizes = input_sizes
         assert len(input_sizes) == 2, 'Two input resolutions need to be specified for ensembles.'
-#       Loss stuff
-#         if self.use_onevsall_loss:
-#             assert self.label_smoothing > 0, 'One-vs-all loss requires label smoothing larger than 0.'
-#             self.criterion = criterions.BCEWithLogitsLoss2().cuda()
-#         elif self.label_smoothing > 0:
-#             self.criterion = criterions.KLDivLoss2()
-#         else:
+
+        # Loss
         self.criterion = nn.CrossEntropyLoss()
     
         self.checkpoint = checkpoint
         self.gpu = gpu
 
-    	# Initialise metrics
-        self.top1_train_accuracy = torchmetrics.Accuracy(top_k=1)
-        self.top3_train_accuracy = torchmetrics.Accuracy(top_k=3)
-
-        self.top1_val_accuracy = torchmetrics.Accuracy(top_k=1)  
-        self.top3_val_accuracy = torchmetrics.Accuracy(top_k=3) 
-
-    def forward(self, x):
-        input_incept = F.interpolate(x, (self.input_sizes[0], self.input_sizes[0]), mode='bilinear') 
-        input_resnet = F.interpolate(x, (self.input_sizes[1], self.input_sizes[1]), mode='bilinear') 
-        return (self.modelIncept(input_incept) + self.modelResnet(input_resnet)) / 2
-
-    def configure_optimizers(self):
-
-        optimiser = torch.optim.SGD(self.parameters(), lr=0.0045, momentum=0.9, weight_decay=0.0001)
-        return optimiser
-
-    def training_step(self, batch, batch_idx):
-
-        data, targets = batch
-        preds = self(data)
-        loss = self.criterion(preds, targets)
-        
-        top1_train_acc = self.top1_train_accuracy(preds, targets)
-        top3_train_acc = self.top3_train_accuracy(preds, targets)
-         
-        self.log('top1_train_acc', top1_train_acc, logger=False, on_epoch=False, on_step=True, prog_bar=True)
-        self.log('top3_train_acc', top3_train_acc, logger=False, on_epoch=False, on_step=True, prog_bar=True)
-        self.log('train_loss', loss, logger=True, on_epoch=True, on_step=True)
-        
-        return {"loss": loss, "logs": {"train_loss": loss, "top1_train_acc": top1_train_acc, "top3_train_acc": top3_train_acc}}
-
-    def training_epoch_end(self, outputs):
-
-        # Log epoch acc
-        top1_acc = self.top1_train_accuracy.compute()
-        top3_acc = self.top3_train_accuracy.compute()
-        self.log('train_top1_acc_epoch', top1_acc, logger=True, on_epoch=True, on_step=False, prog_bar=False)
-        self.log('train_top3_acc_epoch', top3_acc, logger=True, on_epoch=True, on_step=False, prog_bar=False)  
-
-        # Log epoch loss
-        loss = torch.stack([x['loss'] for x in outputs]).mean()
-        self.log('train_loss_epoch', loss, logger=True, on_epoch=True, on_step=False, prog_bar=True)
-    
-    def validation_step(self, batch, batch_idx):
-  
-        data, targets = batch
-        preds = self(data)
-        loss = self.criterion(preds, targets)
-
-        top1_val_acc = self.top1_val_accuracy(preds, targets)
-        top3_val_acc = self.top3_val_accuracy(preds, targets)
-          
-        self.log('top1_train_acc', top1_val_acc, logger=False, on_epoch=False, on_step=False, prog_bar=True)
-        self.log('top3_train_acc', top3_val_acc, logger=False, on_epoch=False, on_step=False, prog_bar=True)
-        self.log('train_loss', loss, logger=True, on_epoch=True, on_step=True)
-        
-        return {"loss": loss, "logs": {"val_loss": loss, "top1_val_acc": top1_val_acc, "top3_val_acc": top3_val_acc}}
-
-
-    def validation_epoch_end(self, outputs):
-
-        # Log epoch acc
-        top1_acc = self.top1_val_accuracy.compute()
-        top3_acc = self.top3_val_accuracy.compute()
-        self.log('val_top1_acc_epoch', top1_acc, logger=True, on_epoch=True, on_step=False, prog_bar=True)
-        self.log('val_top3_acc_epoch', top3_acc, logger=True, on_epoch=True, on_step=False, prog_bar=True)  
-
-        # Log epoch loss
-        loss = torch.stack([x['loss'] for x in outputs]).mean()
-        self.log('val_loss_epoch', loss, logger=True, on_epoch=True, on_step=False, prog_bar=True)
- 
-
-    def load_model(self):
-
-        """
-        Loads a model from a checkpoint.
-        """
-
         if os.path.isfile(self.checkpoint):
             print("=> loading checkpoint '{}'".format(self.checkpoint))
 
-            if self.gpu:
+            if self.gpu > 0:
                 cuda_device = torch.cuda.current_device()
                 checkpoint = torch.load(self.checkpoint, map_location=lambda storage, loc: storage.cuda(cuda_device))
             else:
                 checkpoint = torch.load(self.checkpoint, map_location=lambda storage, loc: storage)
-
+            
             start_epoch = checkpoint['epoch'] if 'epoch' in checkpoint else 0
             best_prec1 = checkpoint['best_prec1'] if 'best_prec1' in checkpoint else 0
             best_prec3 = checkpoint['best_prec3'] if 'best_prec3' in checkpoint else 0
@@ -182,6 +98,78 @@ class EnsembleModel(pl.LightningModule):
             print("=> no checkpoint found at '{}'".format(self.checkpoint))
 
 
+    	# Initialise metrics
+        self.top1_train_accuracy = torchmetrics.Accuracy(top_k=1)
+        self.top3_train_accuracy = torchmetrics.Accuracy(top_k=3)
+
+        self.top1_val_accuracy = torchmetrics.Accuracy(top_k=1)  
+        self.top3_val_accuracy = torchmetrics.Accuracy(top_k=3) 
+
+    def forward(self, x):
+        input_incept = F.interpolate(x, (self.input_sizes[0], self.input_sizes[0]), mode='bilinear') 
+        input_resnet = F.interpolate(x, (self.input_sizes[1], self.input_sizes[1]), mode='bilinear') 
+        return (self.modelIncept(input_incept) + self.modelResnet(input_resnet)) / 2
+
+    def configure_optimizers(self):
+
+        optimiser = torch.optim.SGD(self.parameters(), lr=0.0045, momentum=0.9, weight_decay=0.0001)
+        return optimiser
+
+    def training_step(self, batch, batch_idx):
+
+        data, targets = batch
+        preds = self(data)
+        loss = self.criterion(preds, targets)
+        
+        top1_train_acc = self.top1_train_accuracy(preds, targets)
+        top3_train_acc = self.top3_train_accuracy(preds, targets)
+         
+        self.log('top1_train_acc', top1_train_acc, logger=False, on_epoch=False, on_step=True, prog_bar=True)
+        self.log('top3_train_acc', top3_train_acc, logger=False, on_epoch=False, on_step=True, prog_bar=True)
+        self.log('train_loss', loss, logger=True, on_epoch=True, on_step=True) 
+        
+        return {"loss": loss, "logs": {"train_loss": loss.detach(), "top1_train_acc": top1_train_acc, "top3_train_acc": top3_train_acc}}
+
+    def training_epoch_end(self, outputs):
+
+        # Log epoch acc
+        top1_acc = self.top1_train_accuracy.compute()
+        top3_acc = self.top3_train_accuracy.compute()
+        self.log('train_top1_acc_epoch', top1_acc, logger=True, on_epoch=True, on_step=False, prog_bar=False)
+        self.log('train_top3_acc_epoch', top3_acc, logger=True, on_epoch=True, on_step=False, prog_bar=False)  
+
+        # Log epoch loss
+        loss = torch.stack([x['loss'] for x in outputs]).mean()
+        self.log('train_loss_epoch', loss, logger=True, on_epoch=True, on_step=False, prog_bar=True)
+    
+    def validation_step(self, batch, batch_idx):
+  
+        data, targets = batch
+        preds = self(data)
+        loss = self.criterion(preds, targets)
+
+        top1_val_acc = self.top1_val_accuracy(preds, targets)
+        top3_val_acc = self.top3_val_accuracy(preds, targets)
+          
+        self.log('top1_train_acc', top1_val_acc, logger=False, on_epoch=False, on_step=False, prog_bar=True)
+        self.log('top3_train_acc', top3_val_acc, logger=False, on_epoch=False, on_step=False, prog_bar=True)
+        self.log('train_loss', loss, logger=True, on_epoch=True, on_step=True)
+        
+        return {"loss": loss, "logs": {"val_loss": loss, "top1_val_acc": top1_val_acc, "top3_val_acc": top3_val_acc}}
+
+
+    def validation_epoch_end(self, outputs):
+
+        # Log epoch acc
+        top1_acc = self.top1_val_accuracy.compute()
+        top3_acc = self.top3_val_accuracy.compute()
+        self.log('val_top1_acc_epoch', top1_acc, logger=True, on_epoch=True, on_step=False, prog_bar=True)
+        self.log('val_top3_acc_epoch', top3_acc, logger=True, on_epoch=True, on_step=False, prog_bar=True)  
+
+        # Log epoch loss
+        loss = torch.stack([x['loss'] for x in outputs]).mean()
+        self.log('val_loss_epoch', loss, logger=True, on_epoch=True, on_step=False, prog_bar=True)
+ 
 def main(args):
 
     splits = [
@@ -190,7 +178,7 @@ def main(args):
             '/home/dl18206/Desktop/mres/summer_project/project/data/dataset/splits/testdata.txt'
             ]
           
-    data_path = '/home/dl18206/Desktop/mres/summer_project/project/data/dataset/frames/rgb'
+    data_path = '/home/dl18206/Desktop/mres/summer_project/project/data/pan_african_dataset/data/rgb'
     ann_path = '/home/dl18206/Desktop/mres/summer_project/project/data/dataset/annotations'
 
     checkpoint = 'species_classification.2019.12.00.pytorch'
@@ -204,7 +192,6 @@ def main(args):
         gpu=False
 
     model = EnsembleModel(num_classes=5266, input_sizes=[560, 560], checkpoint=checkpoint, gpu=gpu)
-    model.load_model()
 
     # Checkpoint callbacks    
     val_acc_checkpoint = ModelCheckpoint(
@@ -218,7 +205,7 @@ def main(args):
 
     # Initialise trainer
     if(gpu):
-        trainer = pl.Trainer(callbacks=[val_acc_checkpoint], gpus=args.gpus, nodes=args.nodes, strategy='ddp')
+        trainer = pl.Trainer(callbacks=[val_acc_checkpoint], gpus=args.gpus, num_nodes=args.nodes, strategy='ddp')
     else:
         trainer = pl.Trainer()
 
